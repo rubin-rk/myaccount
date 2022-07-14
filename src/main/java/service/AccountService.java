57ba9cf6fa89ff9a4b3e25322de1bf06f2c2dd01
rubin-rk <errubinkumar@gmail.com>
2022-07-14 19:40:47 +0530

    Update AccountService.java

--- src/main/java/service/AccountService.java
+++ src/main/java/service/AccountService.java
@@ -10,7 +10,7 @@ import java.util.List;
 public class AccountService {
     @Autowired
     AccountRepository accountRepository;
-    //get
+    //get account details
     public List<Account>getaccount(){
         return (List<Account>) accountRepository.findAll();
 
